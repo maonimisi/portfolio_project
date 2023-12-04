e9d9a6c3692b93f96bb3fc4ebcb8afbc9f58e1a6
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-12-04 09:51:48 +0100

    ALX Portfolio Project

--- forecasting/main/routes.py
+++ forecasting/main/routes.py
@@ -6,7 +6,7 @@ main = Blueprint('main', __name__)
 @main.route("/")
 @main.route("/home")
 def home():
-    return render_template('home.html', title='Home')
+    return render_template('index.html', title='Home')
 
 
 @main.route("/about")
